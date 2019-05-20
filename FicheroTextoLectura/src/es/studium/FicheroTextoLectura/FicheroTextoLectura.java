66cd7d6cd0b7ef5b285f19ffcf9c9317990d05d2
ylak963 <pepitogrilloreturn@gmail.com>
2019-05-20 12:16:01 +0200

    Version 0.0

---
+++ FicheroTextoLectura/src/es/studium/FicheroTextoLectura/FicheroTextoLectura.java
@@ -0,0 +1,46 @@
+package es.studium.FicheroTextoLectura;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FicheroTextoLectura
+{
+	public FicheroTextoLectura() {
+		//Como usaremos FileReader y puede lanzar una excepción 
+		//necesitaremos un bloque try – catch.
+		try 
+		{ 
+			//Origen de los datos en el proyecto anterior..
+			FileReader fr = new FileReader("../FicheroTextoEscritura /Archivo.txt"); 
+			//Buffer de lectura 
+			BufferedReader entrada = new BufferedReader(fr);
+			String s; 
+			//Bucle para sacar la información del archivo 
+			while((s=entrada.readLine())!=null) 
+			{ 
+				System.out.println(s); 
+			} 
+			//Cerrar el objeto entrada 
+			entrada.close();
+			fr.close(); 
+
+		} 
+		catch(FileNotFoundException e) 
+		{ 
+			System.out.println("Archivo NO encontrado"); 
+		} 
+		catch(IOException i) 
+		{
+			System.out.println("Se produjo un error de Archivo"); 
+		} 
+	}
+
+	public static void main(String[] args)
+	{
+		new FicheroTextoLectura();
+
+	}
+
+}
